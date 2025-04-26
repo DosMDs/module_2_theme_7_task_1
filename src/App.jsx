45fa76e259b7a7769481e0a0a7d9f5ce0db2bb36
Oleg Skakunov <dosmds@gmail.com>
2025-04-26 17:01:01 +0300

    переход на контекст. вывод списка

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
-import { useState } from "react";
-import { TodoForm, TodoList } from "./components";
-import { useGetTodos } from "./hooks";
 import styles from "./App.module.css";
+import { TodoForm, TodoList } from "./components";
+import { useTodos } from "./context";
 
 function App() {
-	const [titleToSearch, setTitleToSearch] = useState("");
-	const [error, setError] = useState(null);
-	const [refreshTodosFlag, setRefreshTodosFlag] = useState(false);
+	const { loading, error } = useTodos();
 
-	const refreshTodosList = (result) => {
-		setError(result.error);
-		setRefreshTodosFlag(!refreshTodosFlag);
-	};
-
-	const { todos, isLoading, sortOrder, changeSortOrder } = useGetTodos(
-		titleToSearch,
-		refreshTodosFlag,
-		setError
-	);
+	if (loading) {
+		return <div className={styles.loader} />;
+	}
 
 	return (
 		<div className={styles.todoContainer}>
-			<TodoForm
-				setTitleToSearch={setTitleToSearch}
-				refreshTodosList={refreshTodosList}
-				sortOrder={sortOrder}
-				changeSortOrder={changeSortOrder}
-			/>
-			{error ? (
-				<h1>{error}</h1>
-			) : isLoading ? (
-				<div className={styles.loader} />
-			) : (
-				<TodoList
-					todos={todos}
-					titleToSearch={titleToSearch}
-					refreshTodosList={refreshTodosList}
-				/>
-			)}
+			<TodoForm />
+			{error ? <div className={styles.error}>{error}</div> : <TodoList />}
 		</div>
 	);
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,2 @@
-export * from "./TodoItem/TodoItem";
 export * from "./TodoList/TodoList";
 export * from "./TodoForm/TodoForm";
-export * from "./IconButton/IconButton";
-export * from "./TodoItemDisplay/TodoItemDisplay";
